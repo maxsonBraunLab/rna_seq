63846f485e7f3ece105717edc4e60b3f88cab78a
bioThai <tnguyen31415@gmail.com>
2024-12-18 07:37:05 -0800

    Updated resource allocation in cluster.yaml and wrapper scripts. Updated wrapper scripts for ARC compatibility

--- run_pipeline_singularity.sh
+++ run_pipeline_singularity.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #SBATCH --time 24:00:00
-#SBATCH --partition exacloud
+#SBATCH --partition batch
 #SBATCH --job-name run_pipeline 
 #SBATCH --output=jobs/run_pipeline_%j.log
 
@@ -10,7 +10,6 @@
 # Make sure to do the following before running this script:
 # - conda activate snakemake environment
 # - check that slurm profile for snakemake is set up (use a slurm_singularity profile instead if regular slurm profile has Conda-specific configurations)
-# - check that "jobs" folder exists in the main pipeline directory (if not, mkdir jobs)
 # - add correct paths to indices and fastq folders below
 
 # To run this wrapper, do: sbatch run_pipeline_singularity.sh
@@ -19,13 +18,13 @@
 # add absolute path to folder containing original fastq files (not the symlinks)
 # make sure paths contain NO spaces and NO special bash characters (&,!,?, etc.). Dashes, underscores, and dots in file/folder names are acceptable.
 indices_folder="/home/groups/MaxsonLab/indices"
-fastq_folder="/home/groups/MaxsonLab/nguythai/projects/pipeline_maintenance/Bulk-RNA-seq-pipeline-PE-singularity/Bulk-RNA-seq-pipeline-PE/.test/samples/raw"
+fastq_folder=""
 
 # set the number of jobs to run at a time (no spaces)
-num_jobs=64
+num_jobs=50
 
-# module load singularity before running snakemake
-module load /etc/modulefiles/singularity/current
+# # module load singularity before running snakemake (no longer needed for ARC partitions)
+# module load /etc/modulefiles/singularity/current
 
 # run snakemake pipeline
 # Note: if a separate Snakemake slurm profile for Singularity exists (e.g. slurm_singularity), you can use it instead of the default slurm profile
